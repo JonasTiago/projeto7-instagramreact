0a98450876c3e6013e2f24bb43a51a410539029c
Jonas Tiago <jonastiago95@gmail.com>
2022-09-14 09:44:52 -0300

    Componetes criados

--- src/componetes/Sugestoes.js
+++ src/componetes/Sugestoes.js
@@ -1,12 +1,6 @@
-import Sugestoes from "./Sugestoes";
-import Usuario from "./Usuario";
-
-export default function SideBar() {
+export default function Sugestoes(){
     return (
-        <div class="sidebar">
-          <Usuario />
-
-          {/* <div class="sugestoes">
+        <div class="sugestoes">
             <div class="titulo">
               Sugestões para você
               <div>Ver tudo</div>
@@ -71,16 +65,6 @@ export default function SideBar() {
 
               <div class="seguir">Seguir</div>
             </div>
-          </div> */}
-          <Sugestoes />
-
-          <div class="links">
-            Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma
           </div>
-
-          <div class="copyright">
-            © 2021 INSTAGRAM DO FACEBOOK
-          </div>
-        </div>
     )
 }
