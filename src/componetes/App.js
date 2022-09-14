cf66e050dbb1a853be9e56234a04e650323dc3d4
Jonas Tiago <jonastiago95@gmail.com>
2022-09-14 09:17:01 -0300

    inciando o projeto em react

---
+++ src/componetes/App.js
@@ -0,0 +1,11 @@
+import Corpo from "./Corpo"
+import NaviBar from "./NaviBar"
+
+export default function App() {
+    return (
+        <div>
+            <NaviBar />
+            <Corpo />
+        </div>
+    )
+}
