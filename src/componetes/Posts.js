784ca4d04767afe1e6c1c06fadf7b23dbc3c1fc9
Jonas Tiago <jonastiago95@gmail.com>
2022-09-16 10:36:49 -0300

    post dinâmicos, salvar e dar like, com contagem

--- src/componetes/Posts.js
+++ src/componetes/Posts.js
@@ -1,30 +1,79 @@
-function Post() {
-  const props = [
-    {
-      user: 'meowed',
-      imageUser: 'assets/img/meowed.svg',
-      conteudo: 'assets/img/gato-telefone.svg',
-      imageCurtidas: 'assets/img/respondeai.svg',
-      numeroCurtidas: 101.523
-    },
-    {
-      user: 'barked',
-      imageUser: 'assets/img/barked.svg',
-      conteudo: 'assets/img/dog.svg',
-      imageCurtidas: 'assets/img/adorable_animals.svg',
-      numeroCurtidas: 99.159
-    },
-    {
-      user: 'barked',
-      imageUser: 'assets/img/barked.svg',
-      conteudo: 'assets/img/dog.svg',
-      imageCurtidas: 'assets/img/adorable_animals.svg',
-      numeroCurtidas: 99.159
-    }]
+import { useState } from "react"
+
+function MarcadorSalvo(props) {
+
+  const [marcador, setMarcador] = useState(props.name)
+
+  function salvar() {
+    const salvo = "bookmark"
+    setMarcador(
+      (marcador === "bookmark-outline") ? salvo : "bookmark-outline"
+    )
+
+  }
+  return (
+    <ion-icon name={marcador} onClick={salvar}></ion-icon>
+  )
+}
+
+function ConteudoPost(props) {
+  const [curtida, setCurtida] = useState(props.estadoCurtida)
+  const [curtidaTotal, setCurtidaTotal] = useState(props.numCurtidas)
+
+  function foiCurtido() {
+    const curtido = "heart"
+    setCurtida(
+      curtida === "heart-outline" ? curtido : "heart-outline"
+    )
+    maisCurtidas(curtida)
+  }
+
+  function maisCurtidas(curtidaFinal) {
+
+    curtidaFinal === "heart-outline" ?
+      setCurtidaTotal(
+        curtidaTotal + 1
+      )
+      : setCurtidaTotal(
+        curtidaTotal - 1
+      )
+  }
+
+  return (
+    <>
+      <div class="conteudo">
+        <img src={props.imagem} onClick={foiCurtido} />
+      </div>
+
+      <div class="fundo">
+        <div class="acoes">
+          <div>
+            <ion-icon name={curtida} onClick={foiCurtido} ></ion-icon>
+            <ion-icon name="chatbubble-outline"></ion-icon>
+            <ion-icon name="paper-plane-outline"></ion-icon>
+          </div>
+          <div>
+            <MarcadorSalvo name={'bookmark-outline'} />
+          </div>
+        </div>
+
+        <div class="curtidas">
+          <img src={props.imageCurtidas} />
+          <div class="texto">
+            Curtido por <strong>respondeai</strong> e <strong>outras {curtidaTotal} pessoas</strong>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+function Post(props) {
 
   return (
     <div>
-      {props.map(item =>
+      {props.todosPost.map(item =>
         <div class="post">
           <div class="topo">
             <div class="usuario">
@@ -35,30 +84,7 @@ function Post() {
               <ion-icon name="ellipsis-horizontal"></ion-icon>
             </div>
           </div>
-
-          <div class="conteudo">
-            <img src={item.conteudo} />
-          </div>
-
-          <div class="fundo">
-            <div class="acoes">
-              <div>
-                <ion-icon name="heart-outline"></ion-icon>
-                <ion-icon name="chatbubble-outline"></ion-icon>
-                <ion-icon name="paper-plane-outline"></ion-icon>
-              </div>
-              <div>
-                <ion-icon name="bookmark-outline"></ion-icon>
-              </div>
-            </div>
-
-            <div class="curtidas">
-              <img src={item.imageCurtidas} />
-              <div class="texto">
-                Curtido por <strong>respondeai</strong> e <strong>outras {item.numeroCurtidas} pessoas</strong>
-              </div>
-            </div>
-          </div>
+          <ConteudoPost imagem={item.conteudo} imageCurtidas={item.imageCurtidas} numCurtidas={item.numeroCurtidas} estadoCurtida={'heart-outline'} />
         </div>
 
       )}
@@ -70,10 +96,25 @@ function Post() {
 }
 
 export default function Posts() {
+  const posts = [
+    {
+      user: 'meowed',
+      imageUser: 'assets/img/meowed.svg',
+      conteudo: 'assets/img/gato-telefone.svg',
+      imageCurtidas: 'assets/img/respondeai.svg',
+      numeroCurtidas: 101
+    },
+    {
+      user: 'barked',
+      imageUser: 'assets/img/barked.svg',
+      conteudo: 'assets/img/dog.svg',
+      imageCurtidas: 'assets/img/adorable_animals.svg',
+      numeroCurtidas: 99
+    }]
 
   return (
     <div class="posts">
-      <Post />
+      <Post todosPost={posts} />
     </div>
 
   )
